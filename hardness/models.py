d0c4c64f190ac1e31ab5abf3a42a85a0d1fdf14e
lcharleux <ludovic.charleux@univ-smb.fr>
2018-01-14 11:46:03 +0100

    models cleaning

--- hardness/models.py
+++ hardness/models.py
@@ -113,45 +113,7 @@ def process_2D_indenter_mesh(part):
     mesh.nodes.loc[:, ("sets", "RIGID_NODES")] = (
          mesh.nodes.sets["ALL_NODES"])
   return mesh        
-################################################################################
-# SAMPLE MESH 2D
-################################################################################
-'''
-def sample_mesh_2D(gmsh_path = "gmsh", 
-                   workdir = "./", 
-                   lx = 1., ly = 1., 
-                   r1 = 2., r2 = 1000., 
-                   Nx = 32, Ny = 32,
-                   Nr = 16, Nt = 8, 
-                   geoPath = "sample_mesh_2D", 
-                   algorithm = "delquad",
-                   **kwargs):
-  """
-  Builds an indentation mesh.
-  """
-  q1 = (r2/r1)**(1./Nr) 
-  lcx, lcy = lx / Nx, ly / Ny
-  geo = Template(
-        open(MODPATH + "/templates/models/indentation_2D/indentation_mesh_2D.geo").read())
-  geo = geo.substitute(
-        lx = lx,
-        ly = ly,
-        r1 = r1,
-        r2 = r2,
-        Nx = Nx,
-        Ny = Ny,
-        Nr = Nr,
-        Nt = Nt,
-        q1 = q1)
-  open(workdir + geoPath + ".geo", "w").write(geo)
-  p = subprocess.Popen("{0} -2 -algo {1} {2}".format(gmsh_path, 
-                                                     algorithm,
-                                                     geoPath + ".geo"), 
-                       cwd = workdir, shell=True, stdout = subprocess.PIPE)  
-  trash = p.communicate()
-  mesh = argiope.mesh.read_msh(workdir + geoPath + ".msh")
-  return process_2D_mesh(mesh, **kwargs)
-'''
+
 
 ################################################################################
 # PARTS
