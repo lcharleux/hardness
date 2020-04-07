d159ad6245892e98ba31a4ec1ccb5eeef2ab435a
lcharleux <ludovic.charleux@univ-smb.fr>
2020-04-08 00:12:21 +0200

    added 3D models

--- hardness/models.py
+++ hardness/models.py
@@ -4,7 +4,7 @@ import pandas as pd
 import argiope, hardness
 import os, subprocess, inspect
 from string import Template
-
+import gmsh
 # PATH TO MODULE
 import hardness
 MODPATH = os.path.dirname(inspect.getfile(hardness))
@@ -17,6 +17,10 @@ class Indentation2D(argiope.models.Model, argiope.utils.Container):
   """
   2D indentation class.
   """
+  
+  def __init__(self, friction = 0., *args, **kwargs):
+    self.friction = friction
+    argiope.models.Model.__init__(self, *args, **kwargs)
     
   def write_input(self):
     """
@@ -26,6 +30,7 @@ class Indentation2D(argiope.models.Model, argiope.utils.Container):
                                    indenter_mesh = self.parts["indenter"],
                                    steps = self.steps,
                                    materials = self.materials,
+                                   friction = self.friction,
                                    solver = self.solver,
                                    path = "{0}/{1}.inp".format(self.workdir,
                                                                self.label))
@@ -190,42 +195,6 @@ def process_2D_indenter_mesh(part):
 # PARTS
 ################################################################################  
 
-
-'''
-def conical_indenter_mesh_2D(gmsh_path, workdir, psi= 70.29, 
-                             r1 = 1., r2 = 10., r3 = 100., 
-                             lc1 = 0.1, lc2 = 20., 
-                             rigid = False, 
-                             geoPath = "conical_indenter_2D", 
-                             algorithm = "delquad", 
-                             **kwargs):
-  """
-  Builds a conical indenter mesh.
-  """
-  # Some high lvl maths...
-  psi = np.radians(psi)
-  x2 = r3 * np.sin(psi)
-  y2 = r3 * np.cos(psi)
-  y3 = r3 
-  # Template filling  
-  geo = Template(
-        open(MODPATH + "/templates/models/indentation_2D/conical_indenter_mesh_2D.geo").read())
-  geo = geo.substitute(
-    lc1 = lc1,
-    lc2 = lc2,
-    r1  = r1,
-    r2  = r2,
-    x2  = x2,
-    y2  = y2,
-    y3  = y3)
-  open(workdir + geoPath + ".geo", "w").write(geo)
-  p = subprocess.Popen("{0} -2 -algo {1} {2}".format(gmsh_path, 
-                                                     algorithm,
-                                                     geoPath + ".geo"), 
-                       cwd = workdir, shell=True, stdout = subprocess.PIPE)
-  trash = p.communicate()
-  return process_2D_indenter(mesh, **kwargs)
-'''
 class Sample(argiope.models.Part):
   pass
    
@@ -433,9 +402,284 @@ class SpheroconicalIndenter2D(Indenter2D):
        y6 = y6)
     open(self.workdir + self.file_name + ".geo", "w").write(geo)
   
+# 3D INDENTERS
+
+class Indenter3D(Indenter):
+    """
+    A generic (but empty) 3D indenter metaclass.
+    """
+    pass
+
+
+class PyramidalIndenter3DFull(Indenter3D):
+    """
+    A Full 3D pyramidal indenter class.
+
+    Args:
+    * psi: axis to face angle (in degrees).
+    * Nf: number of faces/edges
+    * Rs: radius of the outer sphere
+    * Rt: tip and edge radius
+    * lc1: characteristic element size at tip.
+    * lc2: characteristic element size far from the tip.
+    * r1: transition radius for the characteristic length. Below r1 from tip, the elements have a constant size. Above r1, the characteristic length is linerarly increased up to lc2 at Rs distance.
+    """
+    def __init__(self, psi= 65., Nf = 3,
+                     Rs = 10., Rt = 1.,
+                     blunt = True, 
+                     lc1 = 0.05, lc2 = 5., 
+                     r1 = 0.5, *args, **kwargs):
+        self.psi = psi
+        self.Nf = Nf
+        self.Rs = Rs
+        self.Rt = Rt
+        self.r1 = r1
+        self.blunt = blunt
+        self.lc1 = lc1
+        self.lc2 = lc2
+        self.r1 = r1
+        super().__init__(**kwargs)
+    
+    def axis_to_edge_angle(self):
+        """
+        Returns the axis to edge angle in degrees.
+        """
+        phi = np.degrees(np.arctan(np.tan(np.radians(self.psi)) 
+              / np.cos(np.pi / self.Nf))) # Axis vs edge
+        return phi
+    
+    def delta_h(self):
+        """
+        Returns the missing tip length delta h.
+        """
+        return self.Rt * (np.sin(np.radians(self.psi))**-1 - 1. )
+    
+    def make_mesh(self, use_gui = False):
+        """
+        Generates the mesh.
+        """
+        # MODEL SETUP
+        model = gmsh.model
+        factory = model.occ
+        gmsh.initialize()
+        gmsh.option.setNumber("General.Terminal", 1)
+        gmsh.option.setNumber("Mesh.Algorithm", 6);
+        model.add("indenter")
+
+        # DATA
+        psi =  np.radians(self.psi) # Axis to face angle
+        Nf = self.Nf # Number of faces
+        Rs = self.Rs
+        Rt = self.Rt
+        blunt = self.blunt
+
+        # MATH
+        phi = np.radians(self.axis_to_edge_angle())
+        deltah = self.delta_h()
+
+        # PYRAMID STUFF
+        theta = np.pi/Nf
+        factory.addCylinder(0., 0., 0., 0., 0., Rs*2, Rs, 1)
+        for i in range(Nf):
+            box = factory.addBox(0., -2*Rs, -2*Rs, 4*Rs, 4*Rs, 4*Rs)
+            factory.rotate([(3, box)], 0., 0., 0., 0., 1., 0., psi)
+            if theta != 0.:
+                factory.rotate([(3, box)], 0., 0., 0., 0., 0., 1., theta)
+            theta+= 2. * np.pi / Nf
+            factory.cut( [(3, 1),], [(3, box),], removeTool = True) 
+        fused = [(3,1)]
+
+        # BlUNTING
+        if blunt:
+            blunt_tags = [(3,5)]
+            factory.addSphere(0., 0., 0., Rt, 5) 
+            theta = 0.
+            for i in range(Nf):
+                cyl = factory.addCylinder(0., 0., 0., 0., 0., 2*Rs, Rt, 10)
+                factory.rotate([(3, cyl)], 0., 0., 0., 0., 1., 0., phi)
+                if theta != 0.:
+                    factory.rotate([(3, cyl)], 0., 0., 0., 0., 0., 1., theta)
+                theta+= 2. * np.pi / Nf
+                blunt_tags = factory.fuse(blunt_tags, [(3, cyl),], 
+                                          removeTool = True)[0] 
+            factory.translate(blunt_tags, 0., 0., Rt)
+            factory.translate([(3, 1)], 0., 0., -deltah)
+            cut_tags = factory.cut([(3, 1),], [blunt_tags], 
+                                   removeTool = False)[0]
+            # FIND WHAT TO REMOVE AND WHAT TO KEEP !
+            factory.synchronize()
+            volumes = np.array(model.getEntities(3))[:, 1]  
+            surfaces = np.array(model.getEntities(2))[:, 1] 
+            curves = np.array(model.getEntities(1))[:, 1] 
+            points =  np.array(model.getEntities(0))[:, 1] 
+            coordinates = np.array([model.getValue( 0, p, []) for p in points])
+            tip_point = points[ coordinates[:,2] == coordinates[:,2].min()][0] 
+            for tag in cut_tags: 
+                points_in_tag =  np.array(model.getBoundary(tag, 
+                                 recursive = True))[:, 1]  
+                if tip_point in points_in_tag: 
+                    factory.remove([tag], recursive = True)
+                else:
+                    keep_tag = tag
+            fused = factory.fuse([keep_tag], [blunt_tags], 
+                                 removeTool = True)[0]
+           
+        # CUT THE BOTTOM
+        factory.addSphere(0., 0., 0., Rs, 100) 
+        factory.intersect( fused, [(3, 100),])
+
+        # MODEL ANALYSIS
+        factory.synchronize()
+        volumes = np.array(model.getEntities(3))[:, 1]  
+        surfaces = np.array(model.getEntities(2))[:, 1] 
+        curves = np.array(model.getEntities(1))[:, 1] 
+        points =  np.array(model.getEntities(0))[:, 1] 
+        coordinates = np.array([model.getValue( 0, p, []) for p in points])
+        top_point = points[ coordinates[:,2] == coordinates[:,2].max()][0] 
+        tip_point = points[ coordinates[:,2] == coordinates[:,2].min()][0] 
+        points_in_surfaces = [
+                np.array(model.getBoundary([(2, s)], recursive = True))[:,1]
+                for s in surfaces]
+        top_surface = surfaces[np.array([top_point in pis 
+                                         for pis in points_in_surfaces])][0]
+
+        # POINTS
+        model.addPhysicalGroup(0, [top_point], 1)
+        model.setPhysicalName(0, 1, "INDENTER_TOP_POINT")
+        model.addPhysicalGroup(0, [tip_point], 2)
+        model.setPhysicalName(0, 2, "INDENTER_TIP_POINT")
+        # SURFACES
+        model.addPhysicalGroup(2, [top_surface], 1)
+        model.setPhysicalName(2, 1, "INDENTER_TOP_SURFACE")
+        model.addPhysicalGroup(2, [s for s in surfaces if s != top_surface], 2)
+        model.setPhysicalName(2, 2, "INDENTER_BOTTOM_SURFACE")
+        # VOLUMES
+        model.addPhysicalGroup(3, volumes, 1)
+        model.setPhysicalName(3, 1, "INDENTER_BULK")
+         
+        # MESH CONTROL
+        model.mesh.field.add("Distance", 1)
+        model.mesh.field.setNumbers(1, "NodesList", [tip_point])
+        model.mesh.field.add("Threshold", 2);
+        model.mesh.field.setNumber(2, "IField", 1);
+        model.mesh.field.setNumber(2, "LcMin", self.lc1)
+        model.mesh.field.setNumber(2, "LcMax", self.lc2)
+        model.mesh.field.setNumber(2, "DistMin", self.r1)
+        model.mesh.field.setNumber(2, "DistMax", Rs)
+
+        model.mesh.field.setAsBackgroundMesh(2)
+         
+         
+        factory.synchronize()
+        model.mesh.generate(3)
+        gmsh.write(self.file_name + ".msh")
+        if use_gui:
+            gmsh.fltk.run()
+        gmsh.finalize()
+        mesh = argiope.mesh.read_msh(self.file_name + ".msh")
+        for eset in ["INDENTER_TOP_POINT", 
+                     "INDENTER_TIP_POINT",
+                     "INDENTER_TOP_SURFACE",
+                     "INDENTER_BOTTOM_SURFACE"]:
+            mesh.element_set_to_node_set(eset)
+            del mesh.elements[("sets", eset, "")]
+        mesh.elements = mesh.elements[mesh.space() == 3]
+        mesh.node_set_to_surface("INDENTER_BOTTOM_SURFACE")
+        self.mesh = mesh  
   
-  
-  
+class TransverseFiberSample3D(Sample):
+    """
+    A Full 3D sample with a tranverse fiber.
+
+    Args:
+    * Rf: fiber radius
+    * Rs: radius of the outer sphere
+    * lc1: characteristic element size at tip.
+    * lc2: characteristic element size far from the tip.
+    * r1: transition radius for the characteristic length. Below r1 from tip, the elements have a constant size. Above r1, the characteristic length is linerarly increased up to lc2 at Rs distance.
+    """
+    def __init__(self, Rf = 1., Rs = 10., 
+                     lc1 = 0.05, lc2 = 5., 
+                     r1 = 0.5, *args, **kwargs):
+        self.Rf = Rf
+        self.Rs = Rs
+        self.r1 = r1
+        self.lc1 = lc1
+        self.lc2 = lc2
+        self.r1 = r1
+        super().__init__(**kwargs)
+       
+    def make_mesh(self, use_gui = False):
+        model = gmsh.model
+        factory = model.occ
+
+        gmsh.initialize()
+        gmsh.option.setNumber("General.Terminal", 1)
+        gmsh.option.setNumber("Mesh.Algorithm", 6);
+        model.add("sample")
+
+        lc1 = self.lc1
+        lc2 = self.lc2
+        Rf = self.Rf
+        Rs = self.Rs
+        r1 = self.r1
+
+        lx, ly, lz = 2.*Rs, 2.*Rs, 2.*Rs # Box shape
+        factory.addCylinder(-2*lx, 0., 0., 4*lx, 0., 0., Rf, 1)
+        factory.addBox(-lx/2., -ly/2., -lz, lx, ly, lz, 2)
+        factory.intersect([(3,2)], [(3,1)], 
+                           removeTool = False, 
+                           removeObject = False, 
+                           tag = 3)
+
+        factory.cut([(3,2)], [(3,1)], 
+                           removeTool = True, 
+                           removeObject = True, 
+                           tag = 4)
+        factory.addSphere(0., 0., 0., Rs, 5)
+        factory.intersect([(3,3)], [(3,5)], 6, removeTool = False )
+        factory.intersect([(3,4)], [(3,5)], 7, removeTool = True )
+
+        # MODEL ANALYSIS
+        factory.synchronize()
+        model.addPhysicalGroup(2, [23, 28, 31], 1)
+        model.setPhysicalName(2, 1, "SAMPLE_TOP_SURFACE")
+        model.addPhysicalGroup(2, [24, 26,27, 29], 2)
+        model.setPhysicalName(2, 2, "SAMPLE_BOTTOM_SURFACE")
+        model.addPhysicalGroup(3, [6], 1)
+        model.setPhysicalName(3, 1, "SAMPLE_FIBER")
+        model.addPhysicalGroup(3, [7], 2)
+        model.setPhysicalName(3, 2, "SAMPLE_BULK")
+
+        # MESH CONTROL
+        top_point = factory.addPoint(0., 0., 0.)
+        model.mesh.field.add("Distance", 1)
+        model.mesh.field.setNumbers(1, "NodesList", [top_point])
+        model.mesh.field.add("Threshold", 2);
+        model.mesh.field.setNumber(2, "IField", 1);
+        model.mesh.field.setNumber(2, "LcMin", lc1)
+        model.mesh.field.setNumber(2, "LcMax", lc2)
+        model.mesh.field.setNumber(2, "DistMin", r1)
+        model.mesh.field.setNumber(2, "DistMax", Rs)
+        model.mesh.field.setAsBackgroundMesh(2)
+
+        # WRITE
+        factory.synchronize()
+        model.mesh.generate(3)
+        #model.mesh.recombine() # CORE DUMP ?
+        file_name = self.file_name
+        gmsh.write(file_name +".msh")
+        if use_gui:
+            gmsh.fltk.run()
+        gmsh.finalize()
+        mesh = argiope.mesh.read_msh(file_name + ".msh")
+        for eset in ["SAMPLE_TOP_SURFACE", 
+                     "SAMPLE_BOTTOM_SURFACE"]:
+            mesh.element_set_to_node_set(eset)
+            del mesh.elements[("sets", eset, "")]
+        mesh.elements = mesh.elements[mesh.space() == 3]
+        mesh.node_set_to_surface("SAMPLE_TOP_SURFACE")
+        self.mesh = mesh    
 
   
 ################################################################################
@@ -508,6 +752,7 @@ def indentation_2D_input(sample_mesh,
                          indenter_mesh,
                          steps, 
                          materials,
+                         friction = 0.,
                          path = None, 
                          element_map = None, 
                          solver = "abaqus",
@@ -528,7 +773,8 @@ def indentation_2D_input(sample_mesh,
         SAMPLE_MESH = sample_mesh.mesh.write_inp(),
         INDENTER_MESH = indenter_mesh.mesh.write_inp(),
         STEPS = "".join([step.get_input() for step in steps]),
-        MATERIALS = "\n".join([m.write_inp() for m in materials]) )
+        MATERIALS = "\n".join([m.write_inp() for m in materials]),
+        FRICTION = friction )
   if path == None:            
     return pattern
   else:
